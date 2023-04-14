015166e0ec73e836d8cedb56a00290f1ab7245cf
AntonMelentiev <info@AntonMelentiev.com>
2023-04-14 17:33:37 +0200

    upd: init for page steps;

--- framework/steps/page.py
+++ framework/steps/page.py
@@ -6,13 +6,6 @@ from pages.google_search.page import GoogleSearchPage
 from pages.python_documentation.page import PythonDocumentationPage
 from pages.python_search.page import PythonSearchPage
 
-pages = {
-    "google_results": GoogleResultsPage,
-    "google_search": GoogleSearchPage,
-    "python_documentation": PythonDocumentationPage,
-    "python_search": PythonSearchPage,
-}
-
 
 class PageStep(metaclass=Singleton):
     google_results: GoogleResultsPage
@@ -24,15 +17,15 @@ class PageStep(metaclass=Singleton):
         self._browser = browser
         self._page = self._browser.new_page()  # TODO: check if it's needed
 
-        for page in pages:
-            setattr(self.__class__, f"_{page}", None)
-            self._set_page_getter(page)
+        for page_name, page_type in self.__annotations__.items():
+            setattr(self.__class__, f"_{page_name}", None)
+            self._set_page_getter(page_name, page_type)
 
-    def _set_page_getter(self, name):
-        setattr(self.__class__, name, property(fget=lambda self: self._get_page(name)))
+    def _set_page_getter(self, page_name, page_type):
+        setattr(self.__class__, page_name, property(fget=lambda self: self._get_page(page_name, page_type)))
 
-    def _get_page(self, name):
-        if getattr(self, f"_{name}") is None:
-            setattr(self, f"_{name}", pages.get(name)(page=self._page))  # TODO: maybe new page should be sent
+    def _get_page(self, page_name, page_type):
+        if getattr(self, f"_{page_name}") is None:
+            setattr(self, f"_{page_name}", page_type(page=self._page))  # TODO: maybe new page should be sent
 
-        return getattr(self, f"_{name}")
+        return getattr(self, f"_{page_name}")
